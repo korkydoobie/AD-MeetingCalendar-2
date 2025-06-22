3a2efceca8049bb45728e09cfe02afa7c75df414
kirkgriffin <krkgrffntn@gmail.com>
2025-06-22 18:09:22 +0800

    refactor(envSetter): removed typeconfig

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,9 +1,6 @@
 <?php
 
+REQUIRE_ONCE __DIR__ . '/../vendor/autoload.php';
+
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
-
-// Distribute the data using array key
-$typeConfig = [
-    'key' => $_ENV['ENV_NAME'],
-];
