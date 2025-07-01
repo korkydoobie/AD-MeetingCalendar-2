9192e4880477730e39f0e79f490be64c7af8986b
kirkgriffin <krkgrffntn@gmail.com>
2025-07-01 21:48:18 +0800

    refactor(tasks.sql): changed sql code

--- database/task.model.sql
+++ database/task.model.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tasks (
-    id SERIAL PRIMARY KEY,
-    meeting_id INTEGER REFERENCES meetings(id),
-    assigned_to INTEGER REFERENCES users(id),
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    meeting_id uuid REFERENCES meetings(id),
+    assigned_to uuid REFERENCES users(id),
     title VARCHAR(150) NOT NULL,
     description TEXT,
     status VARCHAR(50) DEFAULT 'pending',
