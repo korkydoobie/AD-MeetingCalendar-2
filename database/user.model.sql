aaaab7a9298ff87560721812a2a2a36c85fe7178
kirkgriffin <krkgrffntn@gmail.com>
2025-07-01 21:48:37 +0800

    refactor(users.sql): changed sql code

--- database/user.model.sql
+++ database/user.model.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    group_name VARCHAR(50)
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
