545aefdeed3932facd8710c06d5fa53f5e5f4890
kirkgriffin <krkgrffntn@gmail.com>
2025-07-01 21:37:41 +0800

    refactor(meeting_users): changed the database code

--- database/meeting_users.model.sql
+++ database/meeting_users.model.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS meeting_users (
-    meeting_id INTEGER NOT NULL REFERENCES meetings (id),
-    user_id INTEGER NOT NULL REFERENCES users (id),
-    role VARCHAR(50) NOT NULL,
+    meeting_id UUID NOT NULL REFERENCES meetings(id),
+    user_id UUID NOT NULL REFERENCES users(id),
     PRIMARY KEY (meeting_id, user_id)
 );
