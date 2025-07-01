17dd1fa8cd21ad9a44a7078a67d4ce5a9bf30f76
kirkgriffin <krkgrffntn@gmail.com>
2025-07-01 22:13:24 +0800

    feat(dbseederpostgre): added require once for staticData

--- utils/dbSeederPostgresql.util.php
+++ utils/dbSeederPostgresql.util.php
@@ -9,6 +9,7 @@ require 'bootstrap.php';
 
 // 3) envSetter
 require_once UTILS_PATH . '/envSetter.util.php';
+$users = require_once DUMMIES_PATH . '/user.staticData.php';
 
 // ——— Connect to PostgreSQL ———
 $dsn = "pgsql:host={$pgConfig['host']};port={$pgConfig['port']};dbname={$pgConfig['db']}";
@@ -42,3 +43,36 @@ foreach (array_keys($schemaFiles) as $table) {
     $pdo->exec("TRUNCATE TABLE {$table} RESTART IDENTITY CASCADE;");
 }
 echo "✅ Tables truncated successfully.\n";
+
+echo "Seeding users…\n";
+
+// query preparations. NOTE: make sure they matches order and number
+$stmt = $pdo->prepare("
+    INSERT INTO users (username, role, first_name, last_name, password)
+    VALUES (:username, :role, :fn, :ln, :pw)
+");
+
+// plug-in datas from the staticData and add to the database
+foreach ($users as $u) {
+  $stmt->execute([
+    ':username' => $u['username'],
+    ':role' => $u['role'],
+    ':fn' => $u['first_name'],
+    ':ln' => $u['last_name'],
+    ':pw' => password_hash($u['password'], PASSWORD_DEFAULT),
+  ]);
+};
+
+$stmt = $pdo->query("SELECT * FROM users");
+
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($users as $user) {
+    echo "---------------------------\n";
+    echo "User ID: " . $user['id'] . "\n";
+    echo "Username: " . $user['username'] . "\n";
+    echo "First Name: " . $user['first_name'] . "\n";
+    echo "Last Name: " . $user['last_name'] . "\n";
+    echo "Role: " . $user['role'] . "\n";
+    echo "---------------------------\n";
+};
