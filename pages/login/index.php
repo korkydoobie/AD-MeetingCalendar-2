ca59beca4a5266c9ebdefa84c83b9b14fa5dabee
kirkgriffin <krkgrffntn@gmail.com>
2025-07-07 20:33:03 +0800

    initial commit

---
+++ pages/login/index.php
@@ -0,0 +1,13 @@
+<form action="/handlers/authLogin.handler.php" method="POST">
+  <div>
+    <label for="username">Username:</label>
+    <input id="username" name="username" type="text" required class="input">
+  </div>
+
+  <div>
+    <label for="password">Password:</label>
+    <input id="password" name="password" type="password" required class="input">
+  </div>
+
+  <button type="submit">Login</button>
+</form>
