c5902e2eaffe1b9e8bd7b9f7ddd5c76da12a8f12
kirkgriffin <krkgrffntn@gmail.com>
2025-07-08 13:07:25 +0800

    refactor(users.staticdata): changed structure for easy readability

--- staticDatas/dummies/user.staticData.php
+++ staticDatas/dummies/user.staticData.php
@@ -1,4 +1,9 @@
 <?php
 return [
-    ['username' => 'john.smith', 'first_name' => 'John', 'last_name' => 'Smith', 'password' => 'p@ssW0rd1234', 'role' => 'designer'],
+    ['username' => 'john.smith', 
+    'first_name' => 'John', 
+    'last_name' => 'Smith', 
+    'password' => 'p@ssW0rd1234', 
+    'role' => 'designer'
+    ],
 ];
